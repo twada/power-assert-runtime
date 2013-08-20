08131787d1c9fd69dc0a36b3d9bf801fd7bb25f3
Takuto Wada <takuto.wada@gmail.com>
2013-08-21 00:12:42 +0900

    Provide destructive option to instrument target object destructively or not.

--- test/empower_option_test.js
+++ test/empower_option_test.js
@@ -1,42 +1,73 @@
 var q = require('../test_helper').QUnit,
-    instrument = require('../test_helper').instrument,
-    formatter = require('../lib/power-assert-formatter'),
-    enhance = require('../lib/empower').enhance,
-    powerAssertTextLines = [],
-    assert = enhance(q.assert, formatter, function (context, message) {
-        powerAssertTextLines = formatter.format(context);
-    }),
-    espower = require('espower'),
-    esprima = require('esprima');
+    empower = require('../lib/empower');
 
-q.module('path option', {
-    setup: function () {
-        powerAssertTextLines.length = 0;
-    }
+
+q.module('assert object empowerment');
+
+q.test('destructive: false', function () {
+    var assertOk = function (actual, message) {
+        if (!actual) {
+            throw new Error('FakeAssert: assertion failed. ' + message);
+        }
+    };
+    var fakeAssertObject = {
+        ok: assertOk
+    };
+
+    var empoweredAssert = empower(fakeAssertObject, {destructive: false});
+    q.ok(typeof empoweredAssert === 'object');
+    q.ok(typeof empoweredAssert.ok === 'function');
+    q.ok(typeof empoweredAssert._capt === 'function');
+    q.ok(typeof empoweredAssert._expr === 'function');
+    q.notEqual(empoweredAssert, fakeAssertObject);
+    q.notEqual(empoweredAssert.ok, fakeAssertObject.ok);
+    q.equal(fakeAssertObject.ok, assertOk);
+    q.throws(function () {
+        empoweredAssert.ok(false);
+    }, Error, 'Error should be thrown');
 });
 
-q.test('when path option is undefined', function () {
-    var falsyStr = '';
-    assert.ok(eval(instrument('assert(falsyStr);', {destructive: false, source: 'assert(falsyStr);', powerAssertVariableName: 'assert'})));
-    q.deepEqual(powerAssertTextLines, [
-        '# at line: 1',
-        '',
-        'assert(falsyStr);',
-        '       |         ',
-        '       ""        ',
-        ''
-    ]);
+q.test('destructive: true', function () {
+    var assertOk = function (actual, message) {
+        if (!actual) {
+            throw new Error('FakeAssert: assertion failed. ' + message);
+        }
+    };
+    var fakeAssertObject = {
+        ok: assertOk
+    };
+
+    empower(fakeAssertObject, {destructive: true});
+    q.ok(typeof fakeAssertObject === 'object');
+    q.ok(typeof fakeAssertObject.ok === 'function');
+    q.ok(typeof fakeAssertObject._capt === 'function');
+    q.ok(typeof fakeAssertObject._expr === 'function');
+    q.notEqual(fakeAssertObject.ok, assertOk);
+    q.throws(function () {
+        fakeAssertObject.ok(false);
+    }, Error, 'Error should be thrown');
 });
 
-q.test('when path option is provided', function () {
-    var falsyStr = '';
-    assert.ok(eval(instrument('assert(falsyStr);', {destructive: false, source: 'assert(falsyStr);', path: '/path/to/source.js', powerAssertVariableName: 'assert'})));
-    q.deepEqual(powerAssertTextLines, [
-        '# /path/to/source.js:1',
-        '',
-        'assert(falsyStr);',
-        '       |         ',
-        '       ""        ',
-        ''
-    ]);
+
+
+q.module('assert function empowerment');
+
+q.test('destructive: false', function () {
+    var assertOk = function (actual, message) {
+        if (!actual) {
+            throw new Error('FakeAssert: assertion failed. ' + message);
+        }
+    };
+    assertOk.ok = assertOk;
+
+    var empoweredAssert = empower(assertOk, {destructive: false});
+    q.ok(typeof empoweredAssert === 'function');
+    q.ok(typeof empoweredAssert.ok === 'function');
+    q.ok(typeof empoweredAssert._capt === 'function');
+    q.ok(typeof empoweredAssert._expr === 'function');
+    q.notEqual(empoweredAssert, assertOk);
+    q.notEqual(empoweredAssert.ok, assertOk);
+    q.throws(function () {
+        empoweredAssert(false);
+    }, Error, 'Error should be thrown');
 });
