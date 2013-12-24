5ca3be68a596a0358846f90aede79977b139bad2
Takuto Wada <takuto.wada@gmail.com>
2013-12-24 17:05:46 +0900

    Test empower_test.js on Browser too (not automated yet).

--- test/empower_test.js
+++ test/empower_test.js
@@ -1,5 +1,60 @@
-var empower = require('../lib/empower'),
-    weave = require('../test_helper').weave,
+(function (root, factory) {
+    'use strict';
+
+    var dependencies = [
+        '../lib/empower',
+        'espower',
+        'esprima',
+        'escodegen',
+        'assert'
+    ];
+
+    if (typeof define === 'function' && define.amd) {
+        define(dependencies, factory);
+    } else if (typeof exports === 'object') {
+        factory.apply(root, dependencies.map(function (path) { return require(path); }));
+    } else {
+        factory.apply(root, dependencies.map(function (path) {
+            var tokens = path.split('/');
+            return root[tokens[tokens.length - 1]];
+        }));
+    }
+}(this, function (
+    empower,
+    espower,
+    esprima,
+    escodegen,
+    baseAssert
+) {
+    // see: https://github.com/Constellation/escodegen/issues/115
+    if (typeof define === 'function' && define.amd) {
+        escodegen = window.escodegen;
+    }
+
+    var weave = function () {
+        function extractBodyFrom (source) {
+            var tree = esprima.parse(source, {tolerant: true, loc: true, range: true});
+            return tree.body[0];
+        }
+
+        function extractBodyOfAssertionAsCode (node) {
+            var expression;
+            if (node.type === 'ExpressionStatement') {
+                expression = node.expression;
+            }
+            return escodegen.generate(expression.arguments[0], {format: {compact: true}});
+        }
+
+        function applyEspower (line, options) {
+            options = options || {destructive: false, source: line, path: '/path/to/some_test.js', powerAssertVariableName: 'assert'};
+            var tree = extractBodyFrom(line);
+            return espower(tree, options);
+        }
+
+        return function (line, options) {
+            return escodegen.generate(applyEspower(line, options), {format: {compact: true}});
+        };
+    }(),
     fakeFormatter = function (context) {
         return [
             context.location.path,
@@ -9,8 +64,7 @@ var empower = require('../lib/empower'),
 
 
 suite('assertion method with one argument', function () {
-    var baseAssert = require('assert'),
-        assert = empower(baseAssert, fakeFormatter);
+    var assert = empower(baseAssert, fakeFormatter);
 
     test('Identifier', function () {
         var falsy = 0;
@@ -29,8 +83,7 @@ suite('assertion method with one argument', function () {
 
 
 suite('assertion method with two arguments', function () {
-    var baseAssert = require('assert'),
-        assert = empower(baseAssert, fakeFormatter);
+    var assert = empower(baseAssert, fakeFormatter);
 
     test('both Identifier', function () {
         var foo = 'foo', bar = 'bar';
@@ -74,3 +127,5 @@ suite('assertion method with two arguments', function () {
         }
     });
 });
+
+}));
