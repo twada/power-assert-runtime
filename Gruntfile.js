4f176b00f1bacdadbb17e55bc4e75db2cb7f63e4
Takuto Wada <takuto.wada@gmail.com>
2014-03-27 01:16:21 +0900

    Now empower does not depend on Function.prototype.bind

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,12 +52,6 @@ module.exports = function(grunt) {
                     run: true
                 }
             },
-            withoutBind: {
-                src: ['test/test-without-bind.html'],
-                options: {
-                    run: true
-                }
-            },
             amd: {
                 src: ['test/test-amd.html'],
                 options: {
@@ -93,6 +87,6 @@ module.exports = function(grunt) {
     });
 
     grunt.registerTask('unit', ['jshint', 'mochaTest:unit']);
-    grunt.registerTask('test', ['jshint', 'mochaTest:unit', 'mocha:browser', 'mocha:amd', 'mocha:withoutBind']);
+    grunt.registerTask('test', ['jshint', 'mochaTest:unit', 'mocha:browser', 'mocha:amd']);
     grunt.registerTask('coverage', ['mochaTest:coverage']);
 };
