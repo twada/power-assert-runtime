3be63d30c3590d1846e6be78024aaaa23dc31481
Takuto Wada <takuto.wada@gmail.com>
2014-08-03 12:57:36 +0900

    chore(empower): replace obsoleted gulp-clean with del

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp'),
     mocha = require('gulp-mocha'),
     mochaPhantomJS = require('gulp-mocha-phantomjs'),
     webserver = require('gulp-webserver'),
-    clean = require('gulp-clean'),
+    del = require('del'),
     runSequence = require('run-sequence'),
     source = require('vinyl-source-stream'),
     through = require('through2'),
@@ -96,16 +96,12 @@ gulp.task('watch', function () {
     runMochaSimply();
 });
 
-gulp.task('clean_bundle', function () {
-    return gulp
-        .src(config.bundle.destDir, {read: false})
-        .pipe(clean());
+gulp.task('clean_bundle', function (done) {
+    del([config.bundle.destDir], done);
 });
 
-gulp.task('clean_coverage', function () {
-    return gulp
-        .src(config.coverage.filename, {read: false})
-        .pipe(clean());
+gulp.task('clean_coverage', function (done) {
+    del([config.coverage.filename], done);
 });
 
 gulp.task('bundle', ['clean_bundle'], function() {
