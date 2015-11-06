937f55d9307880616c386a101a632e76df548eb7
James Talmage <james@talmage.io>
2015-11-06 02:35:36 -0500

    feat(empower): support async-await state flags
    Adds support for monitoring the `asyn` and `generator` flags set by espower.
    
    See https://github.com/power-assert-js/espower/pull/23

--- test/empower_test.js
+++ test/empower_test.js
@@ -1,19 +1,23 @@
 (function (root, factory) {
     'use strict';
+
     if (typeof define === 'function' && define.amd) {
-        define(['empower', 'espower', 'acorn', 'escodegen', 'assert'], factory);
+        define(['empower', 'espower', 'acorn', 'acorn-es7-plugin', 'babel', 'escodegen', 'assert'], factory);
     } else if (typeof exports === 'object') {
-        factory(require('..'), require('espower'), require('acorn'), require('escodegen'), require('assert'));
+        factory(require('..'), require('espower'), require('acorn'), require('acorn-es7-plugin'), require('babel-core'), require('escodegen'), require('assert'));
     } else {
-        factory(root.empower, root.espower, root.acorn, root.escodegen, root.assert);
+        factory(root.empower, root.espower, root.acorn, root.acornEs7Plugin, root.babel, root.escodegen, root.assert);
     }
 }(this, function (
     empower,
     espower,
     acorn,
+    acornEs7Plugin,
+    babel,
     escodegen,
     baseAssert
 ) {
+    acornEs7Plugin(acorn);
 
     var weave = function (line, patterns) {
         var filepath = '/absolute/path/to/project/test/some_test.js';
@@ -26,19 +30,28 @@
         if (patterns) {
             espowerOptions.patterns = patterns;
         }
-        var jsAST = acorn.parse(line, {ecmaVersion: 6, locations: true, sourceType: 'module', sourceFile: filepath});
+        var jsAST = acorn.parse(line, {ecmaVersion: 7, locations: true, sourceType: 'module', sourceFile: filepath, plugins: {asyncawait: true}});
         var espoweredAST = espower(jsAST, espowerOptions);
-        return escodegen.generate(espoweredAST, {format: {compact: true}});
+        var code = escodegen.generate(espoweredAST, {format: {compact: true}});
+        return babel.transform(code).code
     },
     fakeFormatter = function (context) {
         var events = context.args.reduce(function (accum, arg) {
             return accum.concat(arg.events);
         }, []);
-        return [
+        var output = [
             context.source.filepath,
-            context.source.content,
-            JSON.stringify(events)
-        ].join('\n');
+            context.source.content
+        ];
+        if (context.source.async) {
+            output.push('async:true');
+        }
+        if (context.source.generator) {
+            output.push('generator:true');
+        }
+
+        output.push(JSON.stringify(events));
+        return output.join('\n');
     };
 
 
@@ -303,6 +316,129 @@ suite(JSON.stringify(option) + ' assertion method with two arguments', function 
             }
             baseAssert.equal(e.name, 'AssertionError');
         }
+    });
+});
+
+
+suite(JSON.stringify(option) + ' yield for assertion inside generator', function () {
+    test('yield falsy', function (done) {
+        var falsy = Promise.resolve(0);
+
+        function onError(e) {
+            try {
+                if (!e) {
+                    return done(new Error('Assertion Error should be thrown'));
+                }
+                if (option.modifyMessageOnRethrow) {
+                    baseAssert.equal(e.message, [
+                        'test/some_test.js',
+                        'assert.ok(yield falsy)',
+                        'generator:true',
+                        '[{"value":0,"espath":"arguments/0"}]'
+                    ].join('\n'));
+                }
+                if (option.saveContextOnRethrow) {
+                    baseAssert.deepEqual(e.powerAssertContext, {
+                        "source": {
+                            "content": "assert.ok(yield falsy)",
+                            "filepath": "test/some_test.js",
+                            "generator": true,
+                            "line": 2
+                        },
+                        "args": [
+                            {
+                                "value": 0,
+                                "events": [
+                                    {"value": 0, "espath": "arguments/0"}
+                                ]
+                            }
+                        ]
+                    });
+                }
+                baseAssert.equal(e.name, 'AssertionError');
+            } catch (e) {
+                return done (e);
+            }
+            done();
+        }
+
+        function onResult() {
+            done(new Error('promise should not resolve'));
+        }
+
+        var code = [
+          'function *gen() {',
+          '    assert.ok(yield falsy);',
+          '}',
+          '',
+          'var g = gen();',
+          '',
+          'g.next().value',
+          '  .then((val) => g.next(val))',
+          '  .then(onResult)',
+          '  .catch(onError);'
+        ].join('\n');
+
+        eval(weave(code));
+    });
+});
+
+suite(JSON.stringify(option) + ' await assertion inside async async function', function () {
+    test('yield falsy', function (done) {
+        var falsy = Promise.resolve(0);
+
+        function onError(e) {
+            try {
+                if (!e) {
+                    return done(new Error('Assertion Error should be thrown'));
+                }
+                if (option.modifyMessageOnRethrow) {
+                    baseAssert.equal(e.message, [
+                        'test/some_test.js',
+                        'assert.ok(await falsy)',
+                        'async:true',
+                        '[{"value":0,"espath":"arguments/0"}]'
+                    ].join('\n'));
+                }
+                if (option.saveContextOnRethrow) {
+                    baseAssert.deepEqual(e.powerAssertContext, {
+                        "source": {
+                            "content": "assert.ok(await falsy)",
+                            "filepath": "test/some_test.js",
+                            "async": true,
+                            "line": 2
+                        },
+                        "args": [
+                            {
+                                "value": 0,
+                                "events": [
+                                    {"value": 0, "espath": "arguments/0"}
+                                ]
+                            }
+                        ]
+                    });
+                }
+                baseAssert.equal(e.name, 'AssertionError');
+            } catch (e) {
+                return done (e);
+            }
+            done();
+        }
+
+        function onResult() {
+            done(new Error('promise should not resolve'));
+        }
+
+        var code = [
+          'async function gen() {',
+          '    assert.ok(await falsy);',
+          '}',
+          'gen()',
+          '  .then(onResult)',
+          '  .catch(onError);'
+        ].join('\n');
+
+        eval(weave(code));
     });
 });
 
--- test/rjsconfig.js
+++ test/rjsconfig.js
@@ -5,6 +5,9 @@ var require = {
         escodegen: '../build/escodegen',
         espower: "../node_modules/espower/build/espower",
         assert: '../build/assert',
+        babel: '../node_modules/babel-core/browser',
+        bluebird: '../node_modules/bluebird/js/browser/bluebird',
+        "acorn-es7-plugin": '../build/acorn-es7-plugin',
         mocha: "../bower_components/mocha/mocha",
         requirejs: "../bower_components/requirejs/require",
         "buster-assertions": "../bower_components/buster.js/buster-test"
