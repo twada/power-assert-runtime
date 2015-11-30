9f3f776ab31ce7849326a057f647c8e205a347ff
Takuto Wada <takuto.wada@gmail.com>
2015-12-01 02:12:26 +0900

    0.1.0

--- build/empower-core.js
+++ build/empower-core.js
@@ -7,7 +7,7 @@
  *   author: Takuto Wada <takuto.wada@gmail.com>
  *   contributors: James Talmage
  *   homepage: http://github.com/twada/empower-core
- *   version: 1.1.0
+ *   version: 0.1.0
  * 
  * call-signature:
  *   license: MIT
@@ -34,7 +34,7 @@
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.empowerCore = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw (f.code="MODULE_NOT_FOUND", f)}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 /**
- * empower - Power Assert feature enhancer for assert function/object.
+ * empower-core - Power Assert feature enhancer for assert function/object.
  *
  * https://github.com/twada/empower-core
  *
@@ -56,11 +56,11 @@ var slice = Array.prototype.slice;
  * @param options enhancement options
  * @return enhanced assert function/object
  */
-function empower (assert, options) {
+function empowerCore (assert, options) {
     var typeOfAssert = (typeof assert);
     var enhancedAssert;
     if ((typeOfAssert !== 'object' && typeOfAssert !== 'function') || assert === null) {
-        throw new TypeError('empower argument should be a function or object.');
+        throw new TypeError('empower-core argument should be a function or object.');
     }
     if (isEmpowered(assert)) {
         return assert;
@@ -111,8 +111,8 @@ function isEmpowered (assertObjectOrFunction) {
     return (typeof assertObjectOrFunction._capt === 'function') && (typeof assertObjectOrFunction._expr === 'function');
 }
 
-empower.defaultOptions = defaultOptions;
-module.exports = empower;
+empowerCore.defaultOptions = defaultOptions;
+module.exports = empowerCore;
 
 },{"./lib/capturable":2,"./lib/decorator":4,"./lib/default-options":5,"./lib/define-properties":6,"core-js/library/fn/object/create":12,"xtend/mutable":27}],2:[function(_dereq_,module,exports){
 'use strict';
