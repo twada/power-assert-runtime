d6474f8caa26792025ad2cf7ba9d4f4aaa942974
Takuto Wada <takuto.wada@gmail.com>
2016-04-05 16:10:29 +0900

    test(power-assert-renderer-file): basic test for FileRenderer

---
+++ packages/power-assert-renderer-file/test/test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+delete require.cache[require.resolve('..')];
+var FileRenderer = require('..');
+
+var helper = require('../../../test_helper/helper');
+var assert = helper.assert;
+var transpile = helper.transpile;
+var testRendering = helper.testRendering;
+
+describe('FileRenderer', function () {
+    it('assert(foo === bar)', function () {
+        var foo = 'foo';
+        var bar = 'bar';
+        testRendering(function () {
+            eval(transpile('assert(foo === bar)'));
+        }, [
+            '# test/some_test.js:1'
+        ], [FileRenderer]);
+    });
+});
