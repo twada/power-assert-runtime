f4e3569fec6441d5334b6da258cbe9048c6a76c6
Takuto Wada <takuto.wada@gmail.com>
2016-04-05 16:35:01 +0900

    refactor(power-assert-context-traversal): make test dependencies simpler a bit

--- packages/power-assert-context-traversal/test/test.js
+++ packages/power-assert-context-traversal/test/test.js
@@ -2,22 +2,10 @@
 
 delete require.cache[require.resolve('..')];
 var ContextTraversal = require('..');
+var types = require('babel-types');
 var baseAssert = require('assert');
-var babel = require('babel-core');
-var createEspowerPlugin = require('babel-plugin-espower/create');
-var empower = require('empower-core');
-var assert = empower(baseAssert);
-
-function transpile (code) {
-    return babel.transform(code, {
-        filename: '/absolute/path/to/project/test/some_test.js',
-        plugins: [
-            createEspowerPlugin(babel, {
-                sourceRoot: '/absolute/path/to/project'
-            })
-        ]
-    }).code;
-}
+var assert = require('../../../test_helper/empowered-assert');
+var transpile = require('../../../test_helper/transpile');
 
 describe('ContextTraversal', function () {
     it('assert(foo === bar)', function (done) {
@@ -43,7 +31,6 @@ describe('ContextTraversal', function () {
     });
 });
 
-
 describe('powerAssertContext structure', function () {
 
     it('assert(foo === bar)', function () {
@@ -59,7 +46,7 @@ describe('powerAssertContext structure', function () {
                     line: 1,
                     ast: '{"type":"CallExpression","callee":{"type":"Identifier","name":"assert","range":[0,6]},"arguments":[{"type":"BinaryExpression","operator":"===","left":{"type":"Identifier","name":"foo","range":[7,10]},"right":{"type":"Identifier","name":"bar","range":[15,18]},"range":[7,18]}],"range":[0,19]}',
                     tokens: '[{"type":{"label":"name"},"value":"assert","range":[0,6]},{"type":{"label":"("},"range":[6,7]},{"type":{"label":"name"},"value":"foo","range":[7,10]},{"type":{"label":"==/!="},"value":"===","range":[11,14]},{"type":{"label":"name"},"value":"bar","range":[15,18]},{"type":{"label":")"},"range":[18,19]}]',
-                    visitorKeys: JSON.stringify(babel.types.VISITOR_KEYS)
+                    visitorKeys: JSON.stringify(types.VISITOR_KEYS)
                 },
                 args: [
                     {
@@ -97,7 +84,7 @@ describe('powerAssertContext structure', function () {
                     line: 1,
                     ast: '{"type":"CallExpression","callee":{"type":"MemberExpression","object":{"type":"Identifier","name":"assert","range":[0,6]},"property":{"type":"Identifier","name":"equal","range":[7,12]},"computed":false,"range":[0,12]},"arguments":[{"type":"Identifier","name":"foo","range":[13,16]},{"type":"Identifier","name":"bar","range":[18,21]}],"range":[0,22]}',
                     tokens: '[{"type":{"label":"name"},"value":"assert","range":[0,6]},{"type":{"label":"."},"range":[6,7]},{"type":{"label":"name"},"value":"equal","range":[7,12]},{"type":{"label":"("},"range":[12,13]},{"type":{"label":"name"},"value":"foo","range":[13,16]},{"type":{"label":","},"range":[16,17]},{"type":{"label":"name"},"value":"bar","range":[18,21]},{"type":{"label":")"},"range":[21,22]}]',
-                    visitorKeys: JSON.stringify(babel.types.VISITOR_KEYS)
+                    visitorKeys: JSON.stringify(types.VISITOR_KEYS)
                 },
                 args: [
                     {
