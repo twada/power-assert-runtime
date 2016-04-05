90718881d2340f8c1f75e4cb45d748cb22c6ba92
Takuto Wada <takuto.wada@gmail.com>
2016-04-05 16:11:00 +0900

    test(power-assert-renderer-comparison): basic test for ComparisonRenderer

---
+++ packages/power-assert-renderer-comparison/test/test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+delete require.cache[require.resolve('..')];
+var ComparisonRenderer = require('..');
+var AssertionRenderer = require('power-assert-renderer-assertion');
+
+var helper = require('../../../test_helper/helper');
+var assert = helper.assert;
+var transpile = helper.transpile;
+var testRendering = helper.testRendering;
+
+describe('ComparisonRenderer', function () {
+    it('assert(foo === bar)', function () {
+        var foo = 'foo';
+        var bar = 'bar';
+        testRendering(function () {
+            eval(transpile('assert(foo === bar)'));
+        }, [
+            '',
+            'assert(foo === bar)',
+            '',
+            '--- [string] bar',
+            '+++ [string] foo',
+            '@@ -1,3 +1,3 @@\n-bar\n+foo\n'
+        ], [AssertionRenderer, ComparisonRenderer]);
+    });
+});
