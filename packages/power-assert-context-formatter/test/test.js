429e29c1719e07590c4fa4abd5af0db763dd6ec8
Takuto Wada <takuto.wada@gmail.com>
2016-08-04 20:52:44 +0900

    feat(power-assert-renderer-assertion): show syntax error when there are some parse errors caused by not-supported syntax

--- packages/power-assert-context-formatter/test/test.js
+++ packages/power-assert-context-formatter/test/test.js
@@ -134,6 +134,36 @@ describe('power-assert-context-formatter : reducers option', function () {
         }
     });
 
+    it('degrade gracefully when there are some parse errors caused by not supported syntax', function () {
+        var format = createFormatter({
+            reducers: [
+                appendAst
+            ],
+            renderers: [
+                AssertionRenderer,
+                DiagramRenderer
+            ]
+        });
+        try {
+            var a = 'a';
+            var b = 'b';
+            var obj = { foo: 'FOO', bar: 'BAR' };
+            eval(transpile('assert.deepEqual({ b, ...obj }, { a, ...obj })', false));
+        } catch (e) {
+            var result = format(e.powerAssertContext);
+            baseAssert.equal(result, [
+                '  ',
+                '  assert.deepEqual({ b, ...obj }, { a, ...obj })',
+                '                        ?                       ',
+                '                        ?                       ',
+                '                        SyntaxError: Unexpected token (1:22)',
+                '                                                ',
+                '                                                ',
+                '  '
+            ].join('\n'));
+        }
+    });
+
 });
 
 
--- packages/power-assert-renderer-assertion/test/test.js
+++ packages/power-assert-renderer-assertion/test/test.js
@@ -5,8 +5,10 @@ var AssertionRenderer = require('..');
 var assert = require('../../../test_helper/empowered-assert');
 var transpile = require('../../../test_helper/transpile');
 var testRendering = require('../../../test_helper/test-rendering');
+var appendAst = require('power-assert-context-reducer-ast');
 
 describe('AssertionRenderer', function () {
+
     it('assert(foo === bar)', function () {
         var foo = 'foo';
         var bar = 'bar';
@@ -15,6 +17,27 @@ describe('AssertionRenderer', function () {
         }, [
             '',
             'assert(foo === bar)'
-        ], [AssertionRenderer]);
+        ], { renderers: [AssertionRenderer] });
     });
+
+    it('show syntax error when there are some parse errors caused by not supported syntax', function () {
+        var a = 'a';
+        var b = 'b';
+        var obj = { foo: 'FOO', bar: 'BAR' };
+        testRendering(function () {
+            eval(transpile('assert.deepEqual({ b, ...obj }, { a, ...obj })', false));
+        }, [
+            '',
+            'assert.deepEqual({ b, ...obj }, { a, ...obj })',
+            '                      ?                       ',
+            '                      ?                       ',
+            '                      SyntaxError: Unexpected token (1:22)'
+        ], {
+            reducers: [
+                appendAst
+            ],
+            renderers: [AssertionRenderer]
+        });
+    });
+
 });
