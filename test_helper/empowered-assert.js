7719356b473f51280571014decf719e56f355cd0
Takuto Wada <takuto.wada@gmail.com>
2018-05-15 16:21:38 +0900

    test: use empower-core in monorepo

--- test_helper/empowered-assert.js
+++ test_helper/empowered-assert.js
@@ -1,6 +1,6 @@
 'use strict';
 
 var baseAssert = require('assert');
-var empower = require('empower-core');
+var empower = require('../packages/empower-core');
 
 module.exports = empower(baseAssert);
