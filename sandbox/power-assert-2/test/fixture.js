4908a60828aa70dc6c21a9597bc5df0a79fd8c0e
Takuto Wada <takuto.wada@gmail.com>
2018-11-22 23:41:26 +0900

    style: format with semistandard

--- sandbox/power-assert-2/test/fixture.js
+++ sandbox/power-assert-2/test/fixture.js
@@ -1,111 +1,111 @@
 'use strict';
 
 // require('@babel/polyfill');
-var assert = require('..');
+const assert = require('..');
 
 function willResolve (value) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            resolve(value);
-        }, 100);
-    });
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(value);
+    }, 100);
+  });
 }
 
 it('assert(foo)', function () {
-    var foo = null;
-    assert(foo);
+  const foo = null;
+  assert(foo);
 });
 
 it('assert(foo === bar)', function () {
-    var foo = 'FOO';
-    var bar = 'BAR';
-    assert(foo === bar);
+  const foo = 'FOO';
+  const bar = 'BAR';
+  assert(foo === bar);
 });
 
 it('assert(foo === bar) with message literal', function () {
-    var foo = 'FOO';
-    var bar = 'BAR';
-    assert(foo === bar, 'assertion message');
+  const foo = 'FOO';
+  const bar = 'BAR';
+  assert(foo === bar, 'assertion message');
 });
 
 it('assert(foo === bar) with message expression', function () {
-    var msg = 'assertion';
-    var msg2 = 'message';
-    var foo = 'FOO';
-    var bar = 'BAR';
-    assert(foo === bar, `${msg} ${msg2}`);
+  const msg = 'assertion';
+  const msg2 = 'message';
+  const foo = 'FOO';
+  const bar = 'BAR';
+  assert(foo === bar, `${msg} ${msg2}`);
 });
 
 it('assert.deepStrictEqual(foo, bar)', function () {
-    var foo = 'FOO';
-    var bar = 'BAR';
-    assert.deepStrictEqual(foo, bar);
+  const foo = 'FOO';
+  const bar = 'BAR';
+  assert.deepStrictEqual(foo, bar);
 });
 
 it('assert with await', async function () {
-    var msg = 'good';
-    var msg2 = 'result';
-    assert(await willResolve(msg) === await willResolve(msg2));
+  const msg = 'good';
+  const msg2 = 'result';
+  assert(await willResolve(msg) === await willResolve(msg2));
 });
 
 it('assert.rejects', async function () {
-    var msg = 'good';
-    var msg2 = 'result';
-    await assert.rejects(willResolve(`${msg} : ${msg2}`));
+  const msg = 'good';
+  const msg2 = 'result';
+  await assert.rejects(willResolve(`${msg} : ${msg2}`));
 });
 
 it('assert.rejects func', async function () {
-    var msg = 'good';
-    var msg2 = 'result';
-    var func = function () {
-        return willResolve(`${msg} : ${msg2}`);
-    };
-    await assert.rejects(func);
+  const msg = 'good';
+  const msg2 = 'result';
+  const func = function () {
+    return willResolve(`${msg} : ${msg2}`);
+  };
+  await assert.rejects(func);
 });
 
 it('assert.throws func', function () {
-    var func = function () {
-        return 'does not throw';
-    };
-    assert.throws(func);
+  const func = function () {
+    return 'does not throw';
+  };
+  assert.throws(func);
 });
 
 it('assert.throws func, obj', function () {
-    var func = function () {
-        return 'does not throw';
-    };
-    var matcher = {
-        name: 'TypeError',
-        code: 'ERR_INVALID_ARG_TYPE'
-    };
-    assert.throws(func, matcher);
+  const func = function () {
+    return 'does not throw';
+  };
+  const matcher = {
+    name: 'TypeError',
+    code: 'ERR_INVALID_ARG_TYPE'
+  };
+  assert.throws(func, matcher);
 });
 
 it('assert.throws func(does not throw), func', function () {
-    var func = function () {
-        return 'does not throw';
-    };
-    var validate = function (err) {
-        return ((err instanceof TypeError) && /wrong/.test(err));
-    };
-    assert.throws(func, validate);
+  const func = function () {
+    return 'does not throw';
+  };
+  const validate = function (err) {
+    return ((err instanceof TypeError) && /wrong/.test(err));
+  };
+  assert.throws(func, validate);
 });
 
 it('assert.throws func(throws), func', function () {
-    var func = function () {
-        throw new Error('BOOOM');
-    };
-    var validate = function (err) {
-        return ((err instanceof TypeError) && /wrong/.test(err));
-    };
-    assert.throws(func, validate);
+  const func = function () {
+    throw new Error('BOOOM');
+  };
+  const validate = function (err) {
+    return ((err instanceof TypeError) && /wrong/.test(err));
+  };
+  assert.throws(func, validate);
 });
 
 it('assert.doesNotThrow func', function () {
-    var func = function () {
-        var te = new TypeError('an Error has occurred');
-        te.code = 'ERR_AMBIGUOUS_ARGUMENT';
-        throw te;
-    };
-    assert.doesNotThrow(func);
+  const func = function () {
+    const te = new TypeError('an Error has occurred');
+    te.code = 'ERR_AMBIGUOUS_ARGUMENT';
+    throw te;
+  };
+  assert.doesNotThrow(func);
 });
--- sandbox/power-assert-2/test/poc_test.js
+++ sandbox/power-assert-2/test/poc_test.js
@@ -1,34 +1,34 @@
 'use strict';
 
 delete require.cache[require.resolve('..')];
-var assert = require('..');
-var transpile = require('../transpile');
+const assert = require('..');
+const transpile = require('../transpile');
 
 function willResolve (value) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            resolve(value);
-        }, 10);
-    });
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(value);
+    }, 10);
+  });
 }
 
 it('assert(foo === bar)', function () {
-    var msg = 'assertion';
-    var msg2 = 'message';
-    var foo = 'FOO';
-    var bar = 'BAR';
-    try {
-        eval(transpile('assert(foo === bar, `${msg} ${msg2}`)'), false);
-    } catch (e) {
-        console.log(e.message);
-    }
+  const msg = 'assertion';
+  const msg2 = 'message';
+  const foo = 'FOO';
+  const bar = 'BAR';
+  try {
+    eval(transpile('assert(foo === bar, `${msg} ${msg2}`)'), false);
+  } catch (e) {
+    console.log(e.message);
+  }
 });
 
 it('assert.rejects(prms)', async function () {
-    var prms = willResolve('good');
-    try {
-        await eval(transpile('assert.rejects(prms)'), false);
-    } catch (e) {
-        console.log(e.message);
-    }
+  const prms = willResolve('good');
+  try {
+    await eval(transpile('assert.rejects(prms)'), false);
+  } catch (e) {
+    console.log(e.message);
+  }
 });
